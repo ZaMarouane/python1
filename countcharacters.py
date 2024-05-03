ce6002488b499a7bd52b0bf0962465e7350e6c42
ZaMarouane <165991211+ZaMarouane@users.noreply.github.com>
2024-05-03 19:13:34 +0100

    countcharacters.py

--- countcharacters.py
+++ countcharacters.py
@@ -1,4 +1,9 @@
-#count how many characters in a given word or sentence.
+####################
+#This script counts how many characters are in a given word or sentence.
+#Author: Marouane
+#date: 3/05/2024
+###################
+
 import time
 
 word = input("Enter a word or sentence: ")
@@ -6,7 +11,7 @@ letter = input("Enter the character you want to count: ")
 count = 0
 for i in word:
     if i == letter:
-        count += 1  # this increase the var count by 1 each time i == letter
+        count += 1  # This increase the var count by 1 each time i == letter
 
 print("Calculating")
 time.sleep(2)
